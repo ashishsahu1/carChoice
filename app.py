7d9a9fed11947dee66c911ac1d68ef756a450e87
Ashish Sahu <ashishsahu1999@hotmail.com>
2020-11-28 23:02:46 +0530

    connection setup 1

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
+@app.route('/', methods =["GET", "POST"])
+def predict():
+    if request.method == "POST":
+        name = request.form.get("name") 
+        marketprice = request.form.get("marketPrice") 
+        distance = request.form.get("distance") 
+        age = request.form.get("age") 
+        return name+" "+marketprice+" "+distance+" "+age
     return render_template('index.html')
 
 if __name__ == "__main__":
