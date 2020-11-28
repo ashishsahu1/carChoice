417d2619e79773db3ba412f7d4ab5a36231e9ecf
Ashish Sahu <ashishsahu1999@hotmail.com>
2020-11-29 00:47:38 +0530

    data collection api created

--- app.py
+++ app.py
@@ -1,30 +1,38 @@
 from flask import Flask, render_template, request
 from flask import jsonify
+import pickle
+
+modelUrl = open('random_forest_regression_model.pkl', 'rb')
+model = pickle.load(modelUrl)
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
+# render_template('index.html', n = name)
 @app.route('/predict', methods =["POST","GET"])
 def predict():    
     if request.method == 'POST':
         name = str(request.form['name'])
-    # name = request.form.get("name") 
-    # marketprice = request.form.get("marketPrice") 
-    # distance = request.form.get("distance") 
-    # age = request.form.get("age") 
-        return render_template('index.html', n = name)
+        marketprice = float(request.form["marketPrice"])
+        distance = int(request.form["distance"])
+        age = int(request.form["age"])
+        owner = int(request.form["lastOwner"])
+
+
+
+
+
+
+
+
+
+
+        return render_template('index.html', predict = pred)
     return 'something went wrong'
 
-# @app.route('/res', methods =["GET", "POST"])
-# def result():
-#     return str(name)
 
-@app.route('/api', methods =["GET", "POST"])
-def api():
-    return jsonify({'name':'Jimit','address':'India'})
 
 if __name__ == "__main__":
     app.run(debug = True)
