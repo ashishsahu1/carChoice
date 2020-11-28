ff827b2ca64529b034192bbf3d05c1b29a128a49
Ashish Sahu <ashishsahu1999@hotmail.com>
2020-11-29 01:24:04 +0530

    all data imported

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-# render_template('index.html', n = name)
+
+data =[]
+
 @app.route('/predict', methods =["POST","GET"])
 def predict():    
     if request.method == 'POST':
@@ -19,17 +21,25 @@ def predict():
         distance = int(request.form["distance"])
         age = int(request.form["age"])
         owner = int(request.form["lastOwner"])
+        fuel = str(request.form["fuel"])
+        trans = str(request.form["trans"])
+        seller = str(request.form["seller"])
 
+        if trans=='auto':
+            trans = 0
+        else:
+            trans = 1
 
+        if seller=='dealer':
+            transm = 0
+        else:
+            transm = 1
 
+        data=[[marketprice, distance, owner, age,]]
 
-
-
-
-
-
-
-        return render_template('index.html', predict = pred)
+        return render_template('index.html', n=name, m =marketprice,
+                            d = distance, a =age, o = owner, f=fuel, t=trans,
+                            s=seller)
     return 'something went wrong'
 
 
